ac6b8040a9e396e5a3dbc731bcd2ea45a64e94da
Franc-Brs <francesco.bursi@hotmail.it>
2023-01-30 20:07:03 +0100

    adding component

--- app/project/components.py
+++ app/project/components.py
@@ -1,18 +1,40 @@
 import numpy as np
 from scipy import signal
-import matplotlib.pyplot as plt
+
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Optional, Union
+
+
+@dataclass
+class GeneratorsParameters:
+    """Dataclass for parameters for Generators"""
+
+    amplitude: int
+    frequency: int
+
+
+@dataclass
+class ModulatorsParameters:
+    """Dataclass for parameters for Modulators"""
+
+    multiplier: Optional[int] = None
+    threshold: Optional[int] = None
+
 
 class AbstractGenerator(ABC):
     """
     Abstract class for Generators
     """
-    def __init__(self, amplitude: int, frequency: int, n_of_steps=3000):
-        self.amplitude = amplitude
-        self.frequency = frequency
-        self.n_of_steps = n_of_steps
-        self.t = np.linspace(0, 1, self.n_of_steps, endpoint=False)
-        self.arg = 2 * np.pi * self.frequency * self.t
+
+    def __init__(self, parameters: GeneratorsParameters, n_of_steps=3000):
+        self.amplitude = parameters.amplitude
+        self.arg = (
+            2
+            * np.pi
+            * parameters.frequency
+            * np.linspace(0, 1, n_of_steps, endpoint=False)
+        )
 
     @abstractmethod
     def signal(self):
@@ -22,25 +44,76 @@ class AbstractGenerator(ABC):
         print(self.signal())
 
 
-
 class SquareWave(AbstractGenerator):
 
-    #define this kind of metadata in order to don't use metaclass
-    _name="squarewave"
+    # define this kind of metadata in order to don't use metaclass
+    _name = "squarewave"
 
     def signal(self):
         return self.amplitude * signal.square(self.arg)
 
+
 class SineWave(AbstractGenerator):
 
-    _name="sinewave"
+    _name = "sinewave"
 
     def signal(self):
         return self.amplitude * np.sin(self.arg)
 
+
 class SawToothWave(AbstractGenerator):
 
-    _name="sawtoothwave"
+    _name = "sawtoothwave"
 
     def signal(self):
         return self.amplitude * signal.sawtooth(self.arg)
+
+
+class AbstractModulators(ABC):
+    """
+    Abstract class for Modulators
+    """
+
+    def __init__(
+        self,
+        parameters: ModulatorsParameters,
+        input: Union[SquareWave, SineWave, SawToothWave],
+    ):
+        self.multiplier = parameters.multiplier
+        self.threshold = parameters.threshold
+        self.input = input
+
+    @abstractmethod
+    def modulator(self):
+        raise NotImplementedError
+
+    def info(self):
+        print(self.modulator())
+
+
+class AmplitudeMod(AbstractModulators):
+
+    _name = "amplitudemod"
+
+    def modulator(self):
+        return self.input.signal() * self.multiplier
+
+
+class ThresholdUp(AbstractModulators):
+
+    _name = "thresholdup"
+
+    def modulator(self):
+        a = self.input.signal()
+        a[a > self.threshold] = self.threshold
+        return a
+
+
+class ThresholdDown(AbstractModulators):
+
+    _name = "thresholddown"
+
+    def modulator(self):
+        a = self.input.signal()
+        a[a < self.threshold] = self.threshold
+        return a
--- app/project/test.py
+++ app/project/test.py
@@ -2,12 +2,35 @@ import components
 import inspect
 
 module = __import__("components")
-class_ = getattr(module, 'SquareWave')
-instance = class_(5,12)
-#print(instance.__name__)
+class_ = getattr(module, "SquareWave")
+# instance = class_(5,12)
+# print(instance.__name__)
 
+par = components.GeneratorsParameters(amplitude=5, frequency=12)
+el = components.SquareWave(par)
+el.info()
+par1 = components.ModulatorsParameters(
+    multiplier=10,
+)
+
+el2 = components.AmplitudeMod(parameters=par1, input=el)
+el2.info()
+
+par2 = components.ModulatorsParameters(
+    threshold=2,
+)
+
+el3 = components.ThresholdUp(parameters=par2, input=el)
+el3.info()
+
+el3 = components.ThresholdDown(parameters=par2, input=el)
+el3.info()
+"""
 for name, obj in inspect.getmembers(components):
     if inspect.isclass(obj):
-        #print(obj, name)
+        # print(obj, name)
         if hasattr(obj, "_name"):
             print(obj._name)
+            c = obj(par)
+            c.info()
+"""
